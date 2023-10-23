0e87052546b33a98c18e7eed1a47eaee81595e6c
roneydua <roneyddasilva@gmail.com>
2023-10-23 10:18:23 -0300

    método de recuperação da aceleração

--- modeling/inveser_problem_solution.py
+++ modeling/inveser_problem_solution.py
@@ -20,5 +20,27 @@ my_colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 FIG_L = 6.29
 FIG_A = (90.0) / 25.4
 
-from modeling.mathModelAccel import InverseProblem
-f = h5py.File('teste.hdf5','w')
+
+def recover_acceleration():
+    # accel = AccelModelInertialFrame()
+    fibers_with_length_info = np.array([1, 5, 9, 11])
+    deformation = np.zeros((fibers_with_length_info.size,1))
+    ip = InverseProblem(fibers_with_length_info)
+    f = h5py.File('modeling_data.hdf5', 'a')
+    ff = f['test_of_group']
+    ## Solver inverse problem
+    temp_fiber_l = np.zeros((fibers_with_length_info.size, ff['t'].size))
+    for idx, _f in enumerate(fibers_with_length_info):
+        temp_fiber_l[idx,:] = ff['fiber_len'][_f-1,:]
+    
+    for idx_t,_t in enumerate(tqdm(ff['t'])):
+        ip.compute_inverse_problem_solution(temp_fiber_l[:,idx_t])
+        ip.estimate_f_vector()
+        ff['recover_accel_simple'][:,idx_t] = ip.estimate_ddrm_B()
+        
+    f.close()
+
+
+recover_acceleration()
+
+
